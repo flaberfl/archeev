98364cb7488ded31b19a42af02ae84c9fefa6458
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2022-05-08 19:38:36 +0300

    del

--- src/js/files/scroll/scroll.js
+++ src/js/files/scroll/scroll.js
@@ -24,47 +24,6 @@ export function pageNavigation() {
 	// Если подключен scrollWatcher, подсвечиваем текущий пукт меню
 	document.addEventListener("watcherCallback", pageNavigationAction);
 	// Основная функция
-	// function pageNavigationAction(e) {
-	// 	if (e.type === "click") {
-	// 		const targetElement = e.target;
-	// 		if (targetElement.closest('[data-goto]')) {
-	// 			const gotoLink = targetElement.closest('[data-goto]');
-	// 			const gotoLinkSelector = gotoLink.dataset.goto ? gotoLink.dataset.goto : '';
-	// 			const noHeader = gotoLink.hasAttribute('data-goto-header') ? true : false;
-	// 			const gotoSpeed = gotoLink.dataset.gotoSpeed ? gotoLink.dataset.gotoSpeed : 500;
-	// 			const offsetTop = gotoLink.dataset.gotoTop ? parseInt(gotoLink.dataset.gotoTop) : 0;
-	// 			gotoBlock(gotoLinkSelector, noHeader, gotoSpeed, offsetTop);
-	// 			e.preventDefault();
-	// 		}
-	// 	} else if (e.type === "watcherCallback" && e.detail) {
-	// 		const entry = e.detail.entry;
-	// 		const targetElement = entry.target;
-	// 		// Обработка пунктов навигации, если указано значение navigator подсвечиваем текущий пукт меню
-	// 		if (targetElement.dataset.watch === 'navigator') {
-	// 			const navigatorActiveItem = document.querySelector(`[data-goto]._navigator-active`);
-	// 			let navigatorCurrentItem;
-	// 			if (targetElement.id && document.querySelector(`[data-goto="#${targetElement.id}"]`)) {
-	// 				navigatorCurrentItem = document.querySelector(`[data-goto="#${targetElement.id}"]`);
-	// 			} else if (targetElement.classList.length) {
-	// 				for (let index = 0; index < targetElement.classList.length; index++) {
-	// 					const element = targetElement.classList[index];
-	// 					if (document.querySelector(`[data-goto=".${element}"]`)) {
-	// 						navigatorCurrentItem = document.querySelector(`[data-goto=".${element}"]`);
-	// 						break;
-	// 					}
-	// 				}
-	// 			}
-	// 			if (entry.isIntersecting) {
-	// 				// Видим объект
-	// 				// navigatorActiveItem ? navigatorActiveItem.classList.remove('_navigator-active') : null;
-	// 				navigatorCurrentItem ? navigatorCurrentItem.classList.add('_navigator-active') : null;
-	// 			} else {
-	// 				// Не видим объект
-	// 				navigatorCurrentItem ? navigatorCurrentItem.classList.remove('_navigator-active') : null;
-	// 			}
-	// 		}
-	// 	}
-	// }
 	function pageNavigationAction(e) {
 		if (e.type === "click") {
 			const targetElement = e.target;
